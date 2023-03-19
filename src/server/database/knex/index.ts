4f13d79ea096878253d33d1a284c873ab2d0d91b
Felippe <felippemda@gmail.com>
2023-03-19 20:36:25 -0300

    issue persists

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -4,6 +4,8 @@ import * as envs from "./environments";
 
 let selectedEnv = process.env.NODE_ENV as 'dev' | 'prod' | 'test' 
 
-export const Knex = knex(envs[selectedEnv])
+console.log(selectedEnv)
+
+export const Knex = knex(envs[selectedEnv ?? 'dev'])
 
 
